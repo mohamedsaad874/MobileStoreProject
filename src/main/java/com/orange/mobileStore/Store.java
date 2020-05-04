de699e826db580fa2b78040912c35351ab337fac
arch-msaad <you@example.com>
2020-05-04 13:48:25 +0200

    .

--- src/main/java/com/orange/mobileStore/Store.java
+++ src/main/java/com/orange/mobileStore/Store.java
@@ -7,6 +7,8 @@ public class Store {
     private List<Tablet> tablets;
     private int phone;
     private int tablet;
+    private double price;
+
 
     public void addPhonesToStore(Phone phone) {
         phones.add(phone);
@@ -14,12 +16,67 @@ public class Store {
 
     public void addTabletToStore(Tablet tablet) {tablets.add(tablet);}
 
-    public int sellPhone(int phone ) {
+    public Store( int tabletIn,int phoneIn, double priceIn) {
+        phone = phoneIn;
+        tablet = tabletIn;
+        price = priceIn;
 
-        return phone;
     }
-    public int sellTablet(int tablet) {
 
-        return tablet;
+        public double sellPhone(int phoneIn) {
+
+            while (phoneIn < 0 || phoneIn > phone) {
+                return -0.0;
+
+            }
+            if(phoneIn < phone) {
+
+                this.phone = this.phone - phoneIn;
+
+                double totalPrice = phoneIn * this.price;
+
+                return totalPrice;
+
+            }  else {
+
+                return 0.0;
+
+            }
+
+        }
+
+
+    public double sellTablet(int tabletIn) {
+        while (tabletIn < 0 || tabletIn > tablet) {
+            return -0.0;
+
+        }
+        if(tabletIn < tablet) {
+
+            this.tablet = this.tablet - tabletIn;
+
+            double totalPrice = tabletIn * this.price;
+
+            return totalPrice;
+
+        }  else {
+
+            return 0.0;
+
+        }
+    }
+    public void setPrice(double priceIn) {
+        this.price = priceIn;
+    }
+    public int getPhone() {
+        return this.phone;
+
+    }
+    public int getTablet() {
+        return this.tablet;
+
+    }
+    public double getPrice() {
+        return this.price;
     }
     }
